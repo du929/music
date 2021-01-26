2793fd1179337f32c71d4622db7709ba73f10197
du929 <877940547@qq.com>
2021-01-26 21:54:34 +0800

    代码更新

--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -6,7 +6,39 @@ Page({
    */
   data: {
     cards:[
-      {},{},{},{}
+      {},{}
+    ],
+    singer:[
+      {
+      "id":"101",
+      "name":"薛之谦",
+      "picUrl":"https://p2.music.126.net/1tSJODTpcbZvNTCdsn4RYA==/109951165034950656.jpg?param=130y130"
+      },
+      {
+        "id":"102",
+        "name":"刘大壮 ",
+        "picUrl":"https://p2.music.126.net/-vAAAt4wZ8bAJCkilZVbcQ==/109951163780408942.jpg?param=130y130"
+        },
+      {
+        "id":"103",
+        "name":"林俊杰",
+        "picUrl":"https://p2.music.126.net/-nXVAQB1nsLgBwyJEuwcZQ==/109951165400835887.jpg?param=130y130"
+        },
+        {
+          "id":"104",
+          "name":"程响",
+          "picUrl":"https://p2.music.126.net/la7enmQXserHhZEs_PWi4A==/109951165134946647.jpg?param=130y130"
+          },  
+      {
+          "id":"105",
+          "name":"黄霄雲 ",
+          "picUrl":"https://p2.music.126.net/uqiXrrgddtJGFfNbBCePfw==/109951164935184845.jpg?param=130y130"
+          },  
+      {
+          "id":"104",
+          "name":"是七叔呢 ",
+          "picUrl":"https://p2.music.126.net/MniU-nsBTWnDSsU80PT35A==/109951163731919117.jpg?param=130y130"
+          }
     ]
   },
 
