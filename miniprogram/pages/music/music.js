8e976cbbfe8e234209d398d5dcee0a5cb8520163
du929 <877940547@qq.com>
2021-01-29 11:17:22 +0800

    更新

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -114,6 +114,10 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+    this.setData({
+      playlist:[]
+    })
+    this._getPlaylist()
 
   },
 
@@ -121,7 +125,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+this._getPlaylist()
   },
 
   /**
@@ -136,12 +140,18 @@ Page({
       title: '加载中',
     })
     wx.cloud.callFunction({
-      name:'playlist'
+      name:'music',
+      data:{
+        start:this.data.playlist.length,
+        count:MAX_LIMIT,
+        $url:'playlist'
+      }
     }).then((res) => {
       console.log(res)
       this.setData({
-        playlist: res.result
+        playlist: this.data.playlist.concat(res.result.data)
       })
+      wx.stopPullDownRefresh()
       wx.hideLoading()
     })
   },
