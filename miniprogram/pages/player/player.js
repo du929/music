5bad56fd64adb0d3274130bce9f6a3fe310f0318
du929 <877940547@qq.com>
2021-01-30 17:26:28 +0800

    更新

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,13 +1,15 @@
 // pages/player/player.js
 let musiclist = []
 let playingIndex = 0
+const backgroundAudioManager = wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   picUrl:''
+   picUrl:'',
+   isPlaying : false
   },
 
   /**
@@ -21,6 +23,8 @@ Page({
       this._loadMusicDetail(options.musicId)
     
   },
+
+
   _loadMusicDetail(musicId){
     let music = musiclist[playingIndex]
     console.log(music)
@@ -39,54 +43,49 @@ Page({
     }
   }).then((res) =>{
     console.log(res)
+    const url = res.result.data[0].url
+    if(url === null){
+      wx.showToast({
+        title: '没有权限播放',
+      })
+      backgroundAudioManager.pause()
+      this.setData({
+        isPlaying:false
+      })
+      return
+    }
+    backgroundAudioManager.src = url
+    backgroundAudioManager.title = music.name
+    backgroundAudioManager.coverImgUrl = music.al.picUrl
+    backgroundAudioManager.singer = music.ar[0].name
+    this.setData({
+      isPlaying:true
+    })
+  })
+  wx.hideLoading()
+},
+togglePlaying(){
+  if(this.data.isPlaying){
+    backgroundAudioManager.pause()
+  }else{
+    backgroundAudioManager.play()
+  }
+  this.setData({
+    isPlaying : !this.data.isPlaying
   })
 },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+onPrev(){
+  playingIndex--
+  if(playingIndex < 0){
+    playingIndex = musiclist.length-1
   }
+  this._loadMusicDetail(musiclist[playingIndex].id)
+},
+onNext(){
+  playingIndex++
+  if(playingIndex === musiclist.length){
+    playingIndex = 0
+  }
+  this._loadMusicDetail(musiclist[playingIndex].id)
+}
 })
